11146f55c27c024d9bc69e60a54302825a16c8e1
YumengChen <chen.yum@husky.neu.edu>
2019-12-05 10:26:49 -0500

    builder ?

---
+++ EventGridTrigger/Startup.cs
@@ -0,0 +1,37 @@
+using ExampleFunction;
+using ExampleFunction.Services;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Azure.WebJobs.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using Willezone.Azure.WebJobs.Extensions.DependencyInjection;
+
+[assembly: WebJobsStartup(typeof(Startup))]
+namespace ExampleFunction
+{
+    internal class Startup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder) =>
+            builder.AddDependencyInjection<ServiceProviderBuilder>();
+    }
+
+    internal class ServiceProviderBuilder : IServiceProviderBuilder
+    {
+        private readonly ILoggerFactory _loggerFactory;
+
+        public ServiceProviderBuilder(ILoggerFactory loggerFactory) =>
+            _loggerFactory = loggerFactory;
+
+        public IServiceProvider Build()
+        {
+            var services = new ServiceCollection();
+
+            services.addEventGrid();
+            services.addSendGrid();
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
