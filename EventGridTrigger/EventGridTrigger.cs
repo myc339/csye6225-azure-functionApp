a3341ef2f83bbda7dc3c8e59ad879cd59c68d70c
YumengChen <chen.yum@husky.neu.edu>
2019-12-05 09:13:40 -0500

    create topic by template

--- EventGridTrigger/EventGridTrigger.cs
+++ EventGridTrigger/EventGridTrigger.cs
@@ -18,12 +18,10 @@ namespace azure_functions
     public static class EventGridTrigger
     {
         [FunctionName("EventGridTrigger")]
-        public static void Run([EventGridTrigger]EventGridEvent eventGridEvent, ILogger log,[SendGrid(ApiKey = "GridKey")] out SendGridMessage message)
+        public static void Run([EventGridTrigger]EventGridEvent eventGridEvent, ILogger log,[SendGrid(ApiKey = "GridKey",To="chen.yum39@gmail.com",From = "noreply@myc39.me")] out SendGridMessage message)
         {
             log.LogInformation(eventGridEvent.Data.ToString());
              message = new SendGridMessage();
-             message.AddTo("chen.yum39@gmail.com");
-             message.SetFrom(new EmailAddress("noreply@myc39.me"));
              message.SetSubject("Message Via Azure Event Grid");
                 // message.PlainTextContent= eventGridEvent.Data.ToString();
               message.AddContent("text/plain", "hello world");
