e7b77ed3e95300de61d38e42267c7ad24ec18502
YumengChen <chen.yum@husky.neu.edu>
2019-12-05 14:07:57 -0500

    done

--- EventGridTrigger/EventGridTrigger.cs
+++ EventGridTrigger/EventGridTrigger.cs
@@ -18,13 +18,20 @@ namespace azure_functions
     public static class EventGridTrigger
     {
         [FunctionName("EventGridTrigger")]
-        public static void Run([EventGridTrigger]EventGridEvent eventGridEvent, ILogger log,[SendGrid(ApiKey = "GridKey",To="chen.yum39@gmail.com",From = "noreply@myc39.me")] out SendGridMessage message)
+        public static void Run([EventGridTrigger]EventGridEvent eventGridEvent, ILogger log,[SendGrid(ApiKey = "GridKey",To="chen.yum39@gmail.com",From = "noreply@myc39.me")] out SendGridMessage message
+            ,[CosmosDB(
+                              databaseName: "ToDoList",
+                              collectionName: "Items",
+                              ConnectionStringSetting = "CosmosDBConnection")]out dynamic documen])
         {
             log.LogInformation(eventGridEvent.Data.ToString());
              message = new SendGridMessage();
              message.SetSubject("Message Via Azure Event Grid");
                 // message.PlainTextContent= eventGridEvent.Data.ToString();
               message.AddContent("text/plain", "hello world");
+              String a = "hello";
+              String b = "world";
+              outputDocument = new{a,b};
         }
     }
 }
